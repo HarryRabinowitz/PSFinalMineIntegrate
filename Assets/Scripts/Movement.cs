5e9c7c9313d397e58c8a0cbdfb438e3691f690f6
Alicia <amstott2@gmail.com>
2015-08-05 11:05:33 -0400

    added folders and animation prototype

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	
+	public float speed = 5f;
+	public float turnSpeed = 90f;
+	public ForceMode forceMode;
+	
+	
+	void FixedUpdate ()
+	{
+		float x = Input.GetAxis ("Horizontal"); 
+		float y = Input.GetAxis ("Vertical"); 
+		
+		Rigidbody rbody = GetComponent<Rigidbody> ();
+		rbody.AddRelativeForce (0f, 0f, y * speed * Time.deltaTime, forceMode);
+
+		transform.Translate (0f, 0f, y * speed * Time.deltaTime);
+		transform.Rotate (0f, x * turnSpeed * Time.deltaTime, 0f);
+		
+
+	}
+	
+}
